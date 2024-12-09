ca250013faede1bef6cf7dfa4bd52da4fab52bc1
Konosuke Sakai <konosuke@konosuke.work>
2024-12-10 05:32:49 +0900

    feat(editor.lua): enhance indent-blankline.nvim configuration to support custom rainbow colors for better visual distinction in code indentation

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -24,6 +24,29 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
-    config = true,
+    config = function()
+      local highlight = {
+        "RainbowLove",
+        "RainbowGold",
+        "RainbowRose",
+        "RainbowPine",
+        "RainbowFoam",
+        "RainbowIris",
+      }
+
+      local hooks = require("ibl.hooks")
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        local palette = require("rose-pine.palette")
+
+        vim.api.nvim_set_hl(0, "RainbowLove", { fg = palette.love })
+        vim.api.nvim_set_hl(0, "RainbowGold", { fg = palette.gold })
+        vim.api.nvim_set_hl(0, "RainbowRose", { fg = palette.rose })
+        vim.api.nvim_set_hl(0, "RainbowPine", { fg = palette.pine })
+        vim.api.nvim_set_hl(0, "RainbowFoam", { fg = palette.foam })
+        vim.api.nvim_set_hl(0, "RainbowIris", { fg = palette.iris })
+      end)
+
+      require("ibl").setup({ indent = { highlight = highlight } })
+    end,
   },
 }
