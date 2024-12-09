bb1fe414008250a5ebb2652f315b70340e422e54
Konosuke Sakai <konosuke@konosuke.work>
2024-12-10 03:13:24 +0900

    feat(lsp.lua): enhance mason-lspconfig setup with custom handlers for LSP servers to improve configuration flexibility and maintainability

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,7 +6,15 @@ return {
   },
   {
     "williamboman/mason-lspconfig.nvim",
-    config = true,
+    config = function()
+      require("mason-lspconfig").setup()
+
+      require("mason-lspconfig").setup_handlers({
+        function(server_name)
+          require("lspconfig")[server_name].setup({})
+        end,
+      })
+    end,
   },
   {
     "hrsh7th/nvim-cmp",
