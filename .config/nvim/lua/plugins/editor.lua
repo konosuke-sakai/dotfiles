f411ccabdc81b7c446d150025860be70a3fc6d81
Konosuke Sakai <konosuke@konosuke.work>
2024-12-10 06:39:44 +0900

    feat(editor.lua): add fzf-lua plugin for enhanced fuzzy finding capabilities in Neovim

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -49,4 +49,11 @@ return {
       require("ibl").setup({ indent = { highlight = highlight } })
     end,
   },
+  {
+    "ibhagwan/fzf-lua",
+    dependencies = {
+      { "ibhagwan/fzf-lua" },
+    },
+    opts = {},
+  },
 }
