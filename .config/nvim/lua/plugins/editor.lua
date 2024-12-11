43929b5edf7d3a267fb3bdb270900e4955bcd492
Konosuke Sakai <konosuke@konosuke.work>
2024-12-12 05:40:19 +0900

    feat(editor.lua): add configuration for conform.nvim to support Lua formatting and format on save options

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -126,6 +126,14 @@ return {
   },
   {
     "stevearc/conform.nvim",
-    opts = {},
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+    },
   },
 }
