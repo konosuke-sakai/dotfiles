e7edaf95db6317ad7e5124cd6d57dd5bf8518e37
Konosuke Sakai <konosuke@konosuke.work>
2024-12-13 08:52:44 +0900

    feat(editor.lua): add FTerm.nvim plugin for terminal integration and key mappings to enhance workflow

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -157,4 +157,11 @@ return {
 		},
 		opts = {},
 	},
+	{
+		"numToStr/FTerm.nvim",
+		config = function()
+			vim.keymap.set("n", "<A-i>", '<Cmd>lua require("FTerm").toggle()<CR>')
+			vim.keymap.set("t", "<A-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+		end,
+	},
 }
