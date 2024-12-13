79ec92cc0aef1868f156330d9e5f79898e9f9df4
Konosuke Sakai <konosuke@konosuke.work>
2024-12-13 10:14:51 +0900

    fix(editor.lua): change key mappings for FTerm toggle from <A-i> to <C-[> for better compatibility and ease of use

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -173,8 +173,8 @@ return {
 	{
 		"numToStr/FTerm.nvim",
 		config = function()
-			vim.keymap.set("n", "<A-i>", '<Cmd>lua require("FTerm").toggle()<CR>')
-			vim.keymap.set("t", "<A-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+			vim.keymap.set("n", "<C-[>", '<Cmd>lua require("FTerm").toggle()<CR>')
+			vim.keymap.set("t", "<C-[>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
 		end,
 	},
 }
