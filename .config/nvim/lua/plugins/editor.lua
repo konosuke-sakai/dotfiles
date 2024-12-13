4a22cc1e39277a93884e45f99ad908013578c0a4
Konosuke Sakai <konosuke@konosuke.work>
2024-12-13 15:08:02 +0900

    feat(editor.lua): add key mapping for buffer deletion with <Leader>bd to enhance editor functionality

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -155,6 +155,8 @@ return {
 			{ "<Leader>bh", "<Cmd>split<CR>", desc = "Split horizontal" },
 			{ "<Leader>bv", "<Cmd>vsplit<CR>", desc = "Split vertical" },
 
+			{ "<Leader>bd", "<Cmd>bdelete<CR>", desc = "Delete" },
+
 			{ "<Leader>w", "<Cmd>write<CR>", desc = "Write" },
 		},
 	},
